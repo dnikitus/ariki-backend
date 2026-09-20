7e85cb086e1f9db6dcddb4b9e521270bb38a4334
dnikitus <111586045+dnikitus@users.noreply.github.com>
2026-09-20 18:54:07 +0400

    Ready for production back

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,19 @@
-export default ({ env }: { env: any }) => ({
+module.exports = ({ env }) => ({
   email: {
     config: {
       provider: 'nodemailer',
       providerOptions: {
         host: env('SMTP_HOST', 'smtp.gmail.com'),
         port: env.int('SMTP_PORT', 587),
-        secure: false,
+        secure: false, // false for port 587 (TLS), true for port 465 (SSL)
         auth: {
           user: env('SMTP_USERNAME'),
           pass: env('SMTP_PASSWORD'),
         },
       },
       settings: {
-        defaultFrom: env('SMTP_USERNAME'),
-        defaultTo: env('RECEIVER_EMAIL'),
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_REPLY_TO'),
       },
     },
   },
